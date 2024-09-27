07b973ecedd35e6c9415298dcb20078fb9c51cf1
Mete2004 <2150982@talnet.nl>
2024-09-27 21:13:57 +0200

    onclick function toegevoegd

--- pop-up.js
+++ pop-up.js
@@ -26,3 +26,15 @@ function showModal() {
             }
         }, 3500);
 }
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+  }
+  
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
