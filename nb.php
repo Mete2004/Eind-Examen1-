fd1d6d875180ce3176c2740dd1eaa5ecb6e900ab
Mete2004 <2150982@talnet.nl>
2024-09-27 21:11:41 +0200

    laatste aanpassingen voor het plaatsen en ophalen van bericht

--- nb.php
+++ nb.php
@@ -16,20 +16,8 @@
 
     <?php
 
-    include 'admin.php';
-
-    try{
-   
-    $sql = "INSERT INTO blog (blog_titel, blog_text)
-    VALUES ($title, $message)";
-    // use exec() because no results are returned
-    $conn->exec($sql);
-    echo "New record created successfully";
-    } catch(PDOException $e) {
-    echo $sql . "<br>" . $e->getMessage();
-    }
-
-    $conn = null;
+        include 'admin.php';
+        
     ?>
 
     <header>
@@ -40,6 +28,7 @@
                 </div>
             </div>
         </div>
+        <!-- user story: interne links en navigeren -->
         <div class="buttons-outer-div">
             <div class="buttons-inner-div">
                 <div class="h-button">
@@ -52,14 +41,14 @@
             </div>
             <div class="buttons-inner-div">
                 <div class="button">
-                    <a href="contact.html">
+                    <a href="contact.php">
                         <h2 class="contact-button">Contact</h2>
                     </a>
                 </div>
             </div>
             <div class="buttons-inner-div">
                 <div class="button">
-                    <a href="blog.html">
+                    <a href="blog.php">
                         <h2 class="blog-button">Blog</h2>
                     </a>
                 </div>
@@ -81,40 +70,34 @@
     <section class="blog-form-section">
         <div class="blog-form-wrapper">
             <div class="blog-form-header-div">
+
+            </div>
             
-            </div>
-            <!-- <div class="blog-form-name-outer-div">
-
-            </div> -->
             <div class="blog-form-text-outer-div">
-                <!-- <div class="blog-form-text-inner-div">
-                   
-                </div> -->
-                <form  action="admin.php" method="post" >
-                <label for="fname">Titel:</label><br>
-                <input class="blog-form-name" type="text" id="fname" name="title" value=""><br>
-                <label for="lname">Bericht:</label><br>
-
-                <label for="w3review"></label>
-
-                <textarea id="blog-message" name="message" rows="4" cols="50" placeholder="">
                 
-                </textarea>
-               
-            </div>
-            <div class="blog-form-button-outer-div">
-                <div class="blog-form-button-inner-div">
-                <input class="blog-form-button" type="submit" value="Submit">
-                
-        
-                </div>
-            </div>
+            <!-- user story: blogfunctionaliteit -->
+                <form  action="blog.php" method="post" >
+                    <label for="fname">Titel:</label><br>
+                    <input class="blog-form-name" type="text" id="fname" name="title" value=""><br>
+                    <label for="lname">Bericht:</label><br>
 
-            </form> 
+                    <label for="w3review"></label>
+
+                    <textarea id="blog-message" name="message" rows="4" cols="50" placeholder=""></textarea>
+
+
+                    <div class="blog-form-button-outer-div">
+                      <div class="blog-form-button-inner-div">
+                      <input class="blog-form-button" type="submit" value="Submit">
+                      </div>
+                    </div>
+
+            </form>
 
         </div>
+
+          </div>
     </section>
-    
+
 </body>
 </html>
-
